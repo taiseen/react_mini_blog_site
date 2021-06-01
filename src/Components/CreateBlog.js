dfbfdad0e7e89b893407df91ea54f84b130908b6
taiseen <taiseen.cse@gmail.com>
2021-06-01 23:57:24 +0600

    POST request to JSON server ==> OK

--- src/Components/CreateBlog.js
+++ src/Components/CreateBlog.js
@@ -8,20 +8,36 @@ function CreateBlog() {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('');
+    const [isPending, setIsPending] = useState(false);
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = { title, body, author };
+        setIsPending(true);
 
-        // now send a POST request, to particular end-point of api...
-        
+        // Question :- after collecting data, what should we do with this?
+        // Answer   :- so now send a POST request, to servers particular end-point of api...
+        //             & convert this normal object into JSON formate, 
+        //             so that transfer protocol can process this data. 
+        //             so this is the gate-way for communication with server.
 
+        const endPoint = 'blogs'; // api end-point
+        const url = `http://localhost:8000/${endPoint}`;
 
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(blog),
+            headers: { 'Content-Type': 'application/json' },
+        }).then(() => {
+            console.log('new blog created');
+            setIsPending(false);
+        });
 
-
-
-    }
+        setTitle('');
+        setBody('');
+        setAuthor('');
+    };
 
 
     return (
@@ -55,7 +71,12 @@ function CreateBlog() {
                     <option value="Taiseen">Taiseen</option>
                 </select>
 
-                <button>Submit</button>
+                {
+                    !isPending
+                        ? <button>Add Blog</button>
+                        : <button disabled>Adding Blog...</button>
+                }
+
             </form>
         </div >
     );
